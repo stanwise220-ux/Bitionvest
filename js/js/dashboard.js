103277dd65cf2c14a5ef1e5bb68f17ac54b0a9b2
stanwise220-ux <stanwise220@gmail.com>
2026-07-15 09:53:40 +0100

    Update dashboard.js

--- js/js/dashboard.js
+++ js/js/dashboard.js
@@ -1,1 +1,26 @@
 
+import { auth } from "./firebase.js";
+
+import {
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+logoutBtn.addEventListener("click", async () => {
+
+  try {
+
+    await signOut(auth);
+
+    alert("Logged out successfully.");
+
+    window.location.href = "login.html";
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
+
+});
