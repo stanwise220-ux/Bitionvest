691849a245f9d76c82c21ac75e059fb7af2a2c8e
stanwise220-ux <stanwise220@gmail.com>
2026-07-15 11:20:52 +0100

    Update admin.js

--- js/js/admin.js
+++ js/js/admin.js
@@ -1,1 +1,36 @@
+import { auth, db } from "./firebase.js";
 
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
+
+  if (!user) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  const snap = await getDoc(doc(db, "users", user.uid));
+
+  if (!snap.exists()) {
+    window.location.href = "dashboard.html";
+    return;
+  }
+
+  const data = snap.data();
+
+  if (!data.isAdmin) {
+    alert("Access denied.");
+    window.location.href = "dashboard.html";
+    return;
+  }
+
+  document.body.style.display = "block";
+
+});
